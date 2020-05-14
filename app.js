8a5a8ab433c0e83563bf0128388685a9aff37209
Mateusz Boczarski <matbocz@outlook.com>
2020-05-14 13:29:16 +0200

    Add playerChoice() and computerChoice()

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-const game = {
+const hands = [...document.querySelectorAll(".left img")];
+const computerImg = document.querySelector(".right img");
+
+const gameHands = {
     playerHand: "",
     computerHand: "",
 };
 
-function playerChoice() {
-    game.playerHand = this.dataset.hand;
-    console.log(game.playerHand);
-}
+const playerChoice = function () {
+    gameHands.playerHand = this.dataset.hand;
+    hands.forEach((hand) => (hand.style.boxShadow = ""));
+    this.style.boxShadow = "0 0 0 4px #0015ff";
+    console.log("player: " + gameHands.playerHand);
+};
 
-// const hands = [...document.querySelectorAll('')];
+const computerChoice = function () {
+    let computerChoice = hands[Math.floor(Math.random() * 3)]; // get computer choice
+    gameHands.computerHand = computerChoice.dataset.hand; // set computer choice
+    computerImg.src = computerChoice.src; // set computer img
+    computerImg.style.display = "inline-block"; // display computer img
+    document.getElementById("loader").style.display = "none"; // hide loader
+
+    console.log("comp: " + gameHands.computerHand);
+};
+
+const gameStart = function () {
+};
+
+computerImg.style.display = "none";
+hands.forEach((hand) => hand.addEventListener("click", playerChoice));
+
+// console.log(computerChoice());
+// console.log(document.getElementById('loader'));
